28b6b2dfbdd74fd14d0d71b6e18ec21a86c5733b
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-18 23:39:02 +0100

    ..

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* ------------------------------------------------------------------------------------------------------------ */
+/* carosello */
 const content = document.querySelector(".content");
 const scrollableContainer = document.querySelector(".scrollable-container");
 
@@ -11,3 +13,11 @@ content.addEventListener("scroll", () => {
     scrollableContainer.classList.remove("start");
   }
 });
+
+/* ------------------------------------------------------------------------------------------------------------ */
+/* Search aside + dropdown */
+/* const searchButtonAside = document.getElementById("search-btn-aside");
+searchButtonAside.onclick = function () {
+  const searchBar = document.getElementById("search-bar");
+  searchBar.classList.toggle("clicked");
+}; */
