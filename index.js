91ed2d0c1896ffcfd60c1c7d99053cf0399da957
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-19 09:13:41 +0100

    ....

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const scrollableContainer = document.querySelector(".scrollable-container");
 
 content.addEventListener("scroll", () => {
   const scrollLeft = content.scrollLeft;
-  const maxScrollLeft = content.scrollWidth - content.clientWidth;
 
   if (scrollLeft > 0) {
     scrollableContainer.classList.add("start");
